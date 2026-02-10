69644872e928e924b44e80b1a93505517ab737b8
Martin C. <no email>
2026-02-10 18:20:20 -0500

    typo

--- day07/aoc2025_07.scala
+++ day07/aoc2025_07.scala
@@ -2,20 +2,20 @@ import scala.collection.mutable
 object Main07 {
     def main(args: Array[String]): Unit = {
         val lines = scala.io.Source.fromFile("day07/input07").getLines().toList
-        var beam_indeces = mutable.Map[Int, Long](lines(0).indexOf('S') -> 1)
+        var beam_indices = mutable.Map[Int, Long](lines(0).indexOf('S') -> 1)
         var part1 = 0
         for (line <- lines.tail)
-            val beam_indeces_new = mutable.Map[Int, Long]().withDefaultValue(0)
-            for ((idx, count) <- beam_indeces)
+            val beam_indices_new = mutable.Map[Int, Long]().withDefaultValue(0)
+            for ((idx, count) <- beam_indices)
                 line(idx) match
                     case '^' =>
-                        beam_indeces_new(idx - 1) += count
-                        beam_indeces_new(idx + 1) += count
+                        beam_indices_new(idx - 1) += count
+                        beam_indices_new(idx + 1) += count
                         part1 += 1
                     case '.' =>
-                        beam_indeces_new(idx) += count
-            beam_indeces = beam_indeces_new
+                        beam_indices_new(idx) += count
+            beam_indices = beam_indices_new
         println("Part 1: " + part1)
-        println("Part 2: " + beam_indeces.values.sum)
+        println("Part 2: " + beam_indices.values.sum)
     }
 }
